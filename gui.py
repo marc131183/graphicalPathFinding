561f1cd6e0413ddf423272107fadee54e43a09bb
marc131183 <marcg99@zedat.fu-berlin.de>
2021-07-22 13:46:32 +0200

    improved readability

--- gui.py
+++ gui.py
@@ -65,8 +65,23 @@ class GUI:
             (0, 0, 255),
         ]
         pygame.display.set_caption("graphicalPathFinding")
+        # create squares (playground)
         self.matrix = [
             [0 for i in range(self.NUM_BOXES)] for j in range(self.NUM_BOXES)
+        ]
+        self.squareSize = (self.SIZE - 10) / self.NUM_BOXES
+        self.squares = [
+            [
+                Button(
+                    (0, 0, 0),
+                    5 + i * self.squareSize,
+                    55 + j * self.squareSize,
+                    self.squareSize - 1,
+                    self.squareSize - 1,
+                )
+                for i in range(self.NUM_BOXES)
+            ]
+            for j in range(self.NUM_BOXES)
         ]
         self.buttons = [
             Button((0, 255, 0), 5, 5, self.SIZE / 4 - 5, 40, "Select start"),
@@ -95,72 +110,66 @@ class GUI:
                 "Start",
             ),
         ]
-        self.squareSize = (self.SIZE - 10) / self.NUM_BOXES
-        self.squares = [
-            [
-                Button(
-                    (0, 0, 0),
-                    5 + i * self.squareSize,
-                    55 + j * self.squareSize,
-                    self.squareSize - 1,
-                    self.squareSize - 1,
-                )
-                for i in range(self.NUM_BOXES)
-            ]
-            for j in range(self.NUM_BOXES)
-        ]
-        self.actions = []
         self.start, self.end = None, None
         self.path, self.order = None, None
         self.mainloop()
 
+    def mouseClicked(self, event):
+        # showcase has not started, one of the 4 buttons was clicked
+        if not self.state in [4, 5, 6]:
+            for i, button in enumerate(self.buttons):
+                if button.isOver(event.dict["pos"]):
+                    self.state = i
+        # select start/end/blocked squares
+        if self.state in [0, 1, 2]:
+            for i in range(len(self.matrix)):
+                for j in range(len(self.matrix[i])):
+                    if self.squares[i][j].isOver(event.dict["pos"]):
+                        # mark start/end point, if it already exists, remove the old one
+                        if self.state == 0:
+                            if self.start != None:
+                                self.matrix[self.start[0]][
+                                    self.start[1]
+                                ] = 0
+                            self.start = (i, j)
+                        elif self.state == 1:
+                            if self.end != None:
+                                self.matrix[self.end[0]][self.end[1]] = 0
+                            self.end = (i, j)
+                        self.matrix[i][j] = self.state + 1
+        # user has pressed start, now create graph and execute path finding algorithm
+        elif self.state == 3:
+            graph = MatrixGraph(self.NUM_BOXES, self.NUM_BOXES)
+            for i in range(len(self.matrix)):
+                for j in range(len(self.matrix)):
+                    if self.matrix[i][j] == 3:
+                        graph.removeVertex(i, j)
+
+            _, self.path, self.order = aStar(graph, self.start, self.end)
+            self.order.pop(0)
+            self.order.pop(-1)
+            self.state = 4
+
     def mainloop(self):
         over = False
+        for button in self.buttons:
+            button.draw(self.dis)
         while not over:
             time.sleep(0.1)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     over = True
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if not self.state in [3, 4]:
-                        for i, button in enumerate(self.buttons):
-                            if button.isOver(event.dict["pos"]):
-                                self.state = i
+                    self.mouseClicked(event)
 
-                    if self.state in [0, 1, 2]:
-                        for i in range(len(self.matrix)):
-                            for j in range(len(self.matrix[i])):
-                                if self.squares[i][j].isOver(event.dict["pos"]):
-                                    # mark start/end point, if it already exists, remove the old one
-                                    if self.state == 0:
-                                        if self.start != None:
-                                            self.matrix[self.start[0]][
-                                                self.start[1]
-                                            ] = 0
-                                        self.start = (i, j)
-                                    elif self.state == 1:
-                                        if self.end != None:
-                                            self.matrix[self.end[0]][self.end[1]] = 0
-                                        self.end = (i, j)
-                                    self.matrix[i][j] = self.state + 1
-                    elif self.state == 3:
-                        graph = MatrixGraph(self.NUM_BOXES, self.NUM_BOXES)
-                        for i in range(len(self.matrix)):
-                            for j in range(len(self.matrix)):
-                                if self.matrix[i][j] == 3:
-                                    graph.removeVertex(i, j)
-
-                        _, self.path, self.order = aStar(graph, self.start, self.end)
-                        self.order.pop(0)
-                        self.order.pop(-1)
-                        self.state = 4
-
+            # graph is created and path computed, show order that vertices were visited in
             if self.state == 4:
                 if len(self.order) != 0:
                     x, y = self.order.pop(0)
                     self.matrix[x][y] = 4
                 else:
                     self.state = 5
+            # destination vertex was visited, now show the shortest path
             elif self.state == 5:
                 if len(self.path) != 1:
                     x, y = self.path.pop(0)
@@ -181,8 +190,5 @@ class GUI:
                             self.squareSize - 1,
                         ],
                     )
-
-            for button in self.buttons:
-                button.draw(self.dis)
             pygame.display.update()
         pygame.quit()
